23b343a20820727aec1bc7bec4891537d386e800
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-07-06 18:47:59 +0900

    変幻自在

---
+++ 13_colors/js/script.js
@@ -0,0 +1,10 @@
+const heading = document.querySelector('#heading');
+const keyframes = {
+  color: ['#f66', '#fc2', '#0c6', '#0bd']
+};
+const options = { 
+  duration: 8000,
+  diraction: 'alternate',
+  iterations: Infinity,
+};
+heading.animate(keyframes,options);
